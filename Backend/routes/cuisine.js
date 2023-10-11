693a6304aa1002fe0dd00c0a688cfc5331d29456
Kelyn Wong <kelynwonget@gmail.com>
2023-10-11 19:12:24 +0800

    change naming to keep consistency, return change to array and authentication shifted to server.js

--- Backend/routes/cuisine.js
+++ Backend/routes/cuisine.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/config'); 
-const authenticate = require('../middleware/authenticate');
 
-router.get('/', authenticate, (req, res) => {
+router.get('/', (req, res) => {
   const sql = `SELECT DISTINCT name FROM cuisines`
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ data: rows });
+    res.json(rows);
   });
 });
 
