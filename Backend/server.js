bc56ec9d49909d8d37355d78d429700d51ceef2a
Kelyn Wong <kelynwonget@gmail.com>
2023-10-11 10:51:05 +0800

    modified server.js to include cuisine route

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
-
 const app = express();
+
+// Parse JSON and URL-encoded body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API routes
+const cuisine = require('./routes/cuisines');
+app.use('/api', cuisine);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-});
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
 });
