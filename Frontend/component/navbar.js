76d1a47e02ce8e7e965c5269c7043b05d889f4d3
netteee <lynettetay48@gmail.com>
2023-10-16 13:58:31 +0800

    nav n footer component

---
+++ Frontend/component/navbar.js
@@ -0,0 +1,32 @@
+const navbar = {
+    template: `
+        <div id="nav">
+            <div class="very-top-nav">
+                <div class="search-bar">
+                    <img class="search-icon" src="img/search.png" alt="search-icon">
+                    <input class="search-field" type="search" placeholder="Search for Recipe...">
+                </div>
+                <button type="button" class="decide-btn">Can't decide?</button>
+                <img src="img/profile.png" class="profile" />
+            </div>
+            <img src="img/logo.png" alt="logo" class="logo" />
+            <div class="nav-main">
+                <a href="/home">Home</a>
+                <a href="/home">Browse Recipe</a>
+                <!-- <div class="dropdown">
+                    <p>Browse Recipe</p>
+                    <div class="dropdown-content">
+                        <ul>
+                            <li><a href="/home">lorem ipsum</a></li>
+                            <li><a href="/home">lorem ipsum</a></li>
+                            <li><a href="/home">lorem ipsum</a></li>
+                        </ul>
+                    </div>
+                </div> -->
+                <a href="/">Nearby Grocers</a>
+                <a href="/">Submit Recipe</a>
+            </div>
+        </div>
+    `,
+};
+  
