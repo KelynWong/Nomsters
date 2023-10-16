f7cc2922314a08d99056e6af6d06393de377a1f5
Kelyn Wong <kelynwonget@gmail.com>
2023-10-16 12:15:16 +0800

    added add user fav recipe and delete fav recipe endpoint

--- Backend/routes/maria/user.js
+++ Backend/routes/maria/user.js
@@ -98,4 +98,139 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+router.post('/:userId/recipe/:recipeId', (req, res) => {
+    const userId = req.params.userId;
+    const recipeId = req.params.recipeId;
+
+    // Check if the user exists
+    const checkUserSQL = 'SELECT 1 FROM user WHERE userId = ?';
+    db.query(checkUserSQL, [userId])
+        .then((userResult) => {
+            if (userResult.length === 0) {
+                res.status(404).json({ message: 'User not found' });
+                return Promise.reject('User not found');
+            }
+
+            // Check if the recipe exists
+            const checkRecipeSQL = 'SELECT 1 FROM recipe WHERE recipeId = ?';
+            return db.query(checkRecipeSQL, [recipeId]);
+        })
+        .then((recipeResult) => {
+            if (recipeResult.length === 0) {
+                res.status(404).json({ message: 'Recipe not found' });
+                return Promise.reject('Recipe not found');
+            }
+
+            // Check if the user and recipe exist in the userfavourite table
+            const checkIfExistsSQL = 'SELECT 1 FROM userfavourite WHERE userId = ? AND recipeId = ?';
+            return db.query(checkIfExistsSQL, [userId, recipeId]);
+        })
+        .then((result) => {
+            if (result.length > 0) {
+                res.status(400).json({ message: 'User favorite recipe already added' });
+                return Promise.reject('User favorite recipe already added');
+            }
+
+            // User and recipe do not exist in userfavourite, proceed to insert
+            const insertSQL = 'INSERT INTO userfavourite (userId, recipeId) VALUES (?, ?)';
+            return db.query(insertSQL, [userId, recipeId]);
+        })
+        .then((insertResult) => {
+            if (insertResult.affectedRows === 1) {
+                res.json({ message: 'User favorite recipe added successfully' });
+            } else {
+                res.status(500).json({ message: 'Error adding user favorite recipe' });
+            }
+        })
+        .catch((err) => {
+            if (!res.headersSent) {
+                res.status(500).json({ message: err });
+            }
+        });
+});
+
+router.delete('/:userId/recipe/:recipeIds', (req, res) => {
+    const userId = req.params.userId;
+    const recipeIds = req.body.recipeIds;
+    const recipeIdsArray = recipeIds.split(',').map(d => d.trim()); 
+
+    const sql = `DELETE FROM userfavourite WHERE userId = ? AND recipeId IN (${recipeIdsArray.map(id => `'${id}'`).join(', ')})`;
+    db.query(sql, [userId, recipeId])
+        .then((result) => {
+            if (result.affectedRows === 0) {
+                res.status(500).json({ message: 'Error adding user favourite recipe' });
+            } else {
+                res.json({ message: 'User favourite recipe added successfully' });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err.message });
+        });
+});
+
+router.delete('/:userId/recipe', (req, res) => {
+    const userId = req.params.userId;
+    const recipeIds = req.body.recipeIds.split(',').map(id => id.trim());
+
+    // Check if the user exists
+    const checkUserSQL = 'SELECT 1 FROM user WHERE userId = ?';
+    db.query(checkUserSQL, [userId])
+        .then((userResult) => {
+            if (userResult.length === 0) {
+                res.status(404).json({ message: 'User not found' });
+                return Promise.reject('User not found');
+            }
+
+            // Check if the user favorites exist
+            const checkFavoritesSQL = 'SELECT 1 FROM userfavourite WHERE userId = ? AND recipeId = ?';
+            const checkPromises = recipeIds.map(recipeId => {
+                return db.query(checkFavoritesSQL, [userId, recipeId]);
+            });
+
+            return Promise.all(checkPromises);
+        })
+        .then((favoritesResults) => {
+            const invalidRecipes = favoritesResults.filter(result => result.length === 0);
+
+            if (invalidRecipes.length > 0) {
+                res.status(404).json({ message: 'Invalid recipe(s) in user favorites' });
+                return Promise.reject('Invalid recipe(s) in user favorites');
+            }
+
+            // Check if each recipeId is valid
+            const checkRecipeSQL = 'SELECT 1 FROM recipe WHERE recipeId = ?';
+            const checkRecipePromises = recipeIds.map(recipeId => {
+                return db.query(checkRecipeSQL, [recipeId]);
+            });
+
+            return Promise.all(checkRecipePromises);
+        })
+        .then((recipeResults) => {
+            const invalidRecipes = recipeResults.filter(result => result.length === 0);
+
+            if (invalidRecipes.length > 0) {
+                res.status(404).json({ message: 'Invalid recipe(s) in recipe table' });
+                return Promise.reject('Invalid recipe(s) in recipe table');
+            }
+
+            // Delete user's favorite recipes
+            const deleteSQL = 'DELETE FROM userfavourite WHERE userId = ? AND recipeId IN (?)';
+            return db.query(deleteSQL, [userId, recipeIds]);
+        })
+        .then((deleteResult) => {
+            if (deleteResult.affectedRows === recipeIds.length) {
+                res.json({ message: 'User favorite recipes deleted successfully' });
+            } else {
+                res.status(500).json({ message: 'Error deleting user favorite recipes' });
+            }
+        })
+        .catch((err) => {
+            if (!res.headersSent) {
+                res.status(500).json({ message: err });
+            }
+        });
+});
+
+
+
 module.exports = router;
