7f1a969ec6351038ddcdb145a7465fa6742a4e8f
Kelyn Wong <kelynwonget@gmail.com>
2023-10-16 09:15:26 +0800

    shifted routes for mysql into folder

---
+++ Backend/routes/mysql/cuisine.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/mysqldb');
+
+router.get('/', (req, res) => {
+  const sql = `SELECT DISTINCT name FROM cuisines`
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+module.exports = router;
