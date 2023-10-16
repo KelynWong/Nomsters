6a8a0f5db783b10110a3d26882e4e9d9974cc756
Paul <pbmadhavan.2022@scis.smu.edu.sg>
2023-10-16 11:31:55 +0800

    Maps without styling

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,7 @@ const diet = require('./routes/maria/diet');
 const recipe = require('./routes/maria/recipe');
 const user = require('./routes/maria/user');
 const auth = require('./routes/maria/auth');
+const maps= require('./routes/maria/maps');
 
 // uncomment for localhost
 // const cuisine = require('./routes/mysql/cuisine');
@@ -33,6 +34,7 @@ app.use('/api/user', authenticate, user);
 app.use('/api/cuisine', authenticate, cuisine);
 app.use('/api/dishType', authenticate, dishtype);
 app.use('/api/diet', authenticate, diet);
+app.use('/api/maps', maps)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
