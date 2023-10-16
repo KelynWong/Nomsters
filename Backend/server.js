a877c34f144c45a6ac525beb3d3bf429085eae27
Kelyn Wong <kelynwonget@gmail.com>
2023-10-16 09:46:23 +0800

    added missing code from merging

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 3000;
+const cors=require('cors');
+require('dotenv').config();
+
+const port = 3100;
 const app = express();
 
 // Parse JSON and URL-encoded body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 const authenticate = require('./middleware/authenticate');
 
 // import api routes
-const cuisine = require('./routes/cuisine');
-const dishtype = require('./routes/dishType');
-const diet = require('./routes/diet');
-const recipe = require('./routes/recipe');
-const user = require('./routes/user');
-const auth = require('./routes/auth');
-const maps = require('./routes/maps');
+const cuisine = require('./routes/maria/cuisine');
+const dishtype = require('./routes/maria/dishType');
+const diet = require('./routes/maria/diet');
+const recipe = require('./routes/maria/recipe');
+const user = require('./routes/maria/user');
+const auth = require('./routes/maria/auth');
+
+// uncomment for localhost
+// const cuisine = require('./routes/mysql/cuisine');
+// const dishtype = require('./routes/mysql/dishType');
+// const diet = require('./routes/mysql/diet');
+// const recipe = require('./routes/mysql/recipe');
+// const user = require('./routes/mysql/user');
+// const auth = require('./routes/mysql/auth');
 
 app.use('/api', auth);
 app.use('/api/recipe', authenticate, recipe);
@@ -26,7 +33,7 @@ app.use('/api/user', authenticate, user);
 app.use('/api/cuisine', authenticate, cuisine);
 app.use('/api/dishType', authenticate, dishtype);
 app.use('/api/diet', authenticate, diet);
-app.use('/api/maps',  maps);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
