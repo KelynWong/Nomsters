04fc84d4d01878aab7ce4cc3c7083398b866a898
netteee <lynettetay48@gmail.com>
2023-10-21 12:28:53 +0800

    icons

--- Frontend/component/navbar.js
+++ Frontend/component/navbar.js
@@ -3,11 +3,11 @@ const navbar = {
         <div id="nav">
             <div class="very-top-nav">
                 <div class="search-bar">
-                    <img class="search-icon" src="img/search.png" alt="search-icon">
+                    <img class="search-icon" src="img/icon/search.png" alt="search-icon">
                     <input class="search-field" type="search" placeholder="Search for Recipe...">
                 </div>
                 <button type="button" class="decide-btn">Can't decide?</button>
-                <img src="img/profile.png" class="profile" />
+                <img src="img/icon/profile.png" class="profile" />
             </div>
             <a href="home.html"><img src="img/logo.png" alt="logo" class="logo" /></a>
             <div class="nav-main">
