23af9bc5989cae3533323f80b9ea7b7e94885f6b
Paul <pbmadhavan.2022@scis.smu.edu.sg>
2023-10-25 00:31:18 +0800

    fixed vue js for profile page

--- Frontend/component/navbar.js
+++ Frontend/component/navbar.js
@@ -13,15 +13,15 @@ const navbar = {
                     <input v-model="searchQuery" class="search-field" type="search" placeholder="Search for Recipe..." @keyup.enter="this.$emit('search', searchQuery)">
                 </div>
                 <button type="button" class="decide-btn" @click="showHideModal">Can't decide?</button>
-                <img src="img/icon/profile.png" class="profile" />
+                <a href="./profile.html"><img src="img/icon/profile.png" class="profile" /></a>
             </div>
             <a href="home.html"><img src="img/logo.png" alt="logo" class="logo" /></a>
             <div class="nav-main">
-                <a href="home.html">Home</a>
-                <a href="allRecipe.html">Browse Recipe</a>
-                <a href="grocer.html">Nearby Grocers</a>
-                <a href="submit.html">Submit Recipe</a>
-                <a href="favourites.html">Favourites</a>
+                <a href="./home.html">Home</a>
+                <a href="./allRecipe.html">Browse Recipe</a>
+                <a href="./grocer.html">Nearby Grocers</a>
+                <a href="./submit.html">Submit Recipe</a>
+                <a href="./favourites.html">Favourites</a>
             </div>
         </div>
     `,
