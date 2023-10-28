9409c96502254fc6d8f7abe9eb63700be82444ee
Kelyn Wong <kelynwonget@gmail.com>
2023-10-28 21:26:57 +0800

    change recipeID to recipeId for consistency

--- Backend/routes/maria/mealprep.js
+++ Backend/routes/maria/mealprep.js
@@ -10,13 +10,13 @@ router.get('/', (req, res) => {
    var num_days=req.query.num_days;
    var budget_day=parseInt(req.query.budget_day);
    var budget_meal=(budget_day/(num_meals+(num_snacks/2)))
- var calorie_per_dish_max = calorie_required /(num_meals+(num_snacks/2)) + 100;
-var calorie_per_dish_min = calorie_required /(num_meals+(num_snacks/2)) - 100;
-var calorie_per_snack_max = (calorie_required / (num_meals+(num_snacks/2)))/2 + 50;
+ var calorie_per_dish_max = calorie_required /(num_meals+(num_snacks/2)) + 150;
+var calorie_per_dish_min = calorie_required /(num_meals+(num_snacks/2)) - 50;
+var calorie_per_snack_max = (calorie_required / (num_meals+(num_snacks/2)))/2 + 100;
 var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 50;
 
    if(!diets){
-    var sql = `(SELECT DISTINCT recipeID, title,image, servings, round(pricePerServing/100,1) as pricePerServing_dollar, readyInMinutes, summary,round(fat/servings) as fatPerServing,round(carbohydrates/servings) as carbsPerServing,
+    var sql = `(SELECT DISTINCT recipeId, title,image, servings, round(pricePerServing/150,1) as pricePerServing_dollar, readyInMinutes, summary,round(fat/servings) as fatPerServing,round(carbohydrates/servings) as carbsPerServing,
     CASE
       WHEN (protein / servings) > 1000 THEN ROUND((protein / servings) / 100, 1)
       ELSE ROUND((protein / servings) / 20, 1)
@@ -24,11 +24,11 @@ var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 
       ROUND(calories / servings, 1) AS calories_per_serving
       FROM recipe
       WHERE (calories / servings) BETWEEN ${calorie_per_snack_min} AND ${calorie_per_snack_max}
-      AND pricePerServing/100 <= ${budget_meal}/2
+      AND pricePerServing/150 <= ${budget_meal}/2
       ORDER BY RAND()
       LIMIT ${num_snacks})
       Union
-    (SELECT DISTINCT recipeID, title,image, servings, round(pricePerServing/100,1) as pricerPerServing_dollar, readyInMinutes, summary,round(fat/servings) as fatPerServing,round(carbohydrates/servings) as carbsPerServing,
+    (SELECT DISTINCT recipeId, title,image, servings, round(pricePerServing/150,1) as pricerPerServing_dollar, readyInMinutes, summary,round(fat/servings) as fatPerServing,round(carbohydrates/servings) as carbsPerServing,
     CASE
     WHEN (protein / servings) > 1000 THEN ROUND((protein / servings) / 100, 1)
     ELSE ROUND((protein / servings) / 20, 1)
@@ -36,7 +36,7 @@ var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 
     ROUND(calories / servings, 1) AS calories_per_serving
     FROM recipe
     WHERE (calories / servings) BETWEEN ${calorie_per_dish_min} AND ${calorie_per_dish_max}
-    AND pricePerServing/100 <= ${budget_meal}
+    AND pricePerServing/150 <= ${budget_meal}
      ORDER BY RAND()
     LIMIT ${num_meals})
   `;
@@ -49,7 +49,7 @@ var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 
       });}
       
       else{ 
-        var sql = `(SELECT DISTINCT recipeID, title, image, servings, round(pricePerServing/100, 1) as pricePerServing_dollar, readyInMinutes, summary, round(fat/servings) as fatPerServing, round(carbohydrates/servings) as carbsPerServing,
+        var sql = `(SELECT DISTINCT recipeId, title, image, servings, round(pricePerServing/150, 1) as pricePerServing_dollar, readyInMinutes, summary, round(fat/servings) as fatPerServing, round(carbohydrates/servings) as carbsPerServing,
     CASE
       WHEN (protein / servings) > 1000 THEN ROUND((protein / servings) / 100, 1)
       ELSE ROUND((protein / servings) / 20, 1)
@@ -57,12 +57,12 @@ var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 
     ROUND(calories / servings, 1) AS calories_per_serving
     FROM recipe
     WHERE (calories / servings) BETWEEN ${calorie_per_snack_min} AND ${calorie_per_snack_max}
-    AND (pricePerServing/100) <= ${budget_meal}/2
-    AND recipeID IN (SELECT recipeID FROM diets WHERE name = '${diets}')
+    AND (pricePerServing/150) <= ${budget_meal}/2
+    AND recipeId IN (SELECT recipeId FROM diets WHERE name = '${diets}')
     ORDER BY RAND()
     LIMIT ${num_snacks})
     UNION
-    (SELECT DISTINCT recipeID, title, servings, image, round(pricePerServing/100, 1) as pricerPerServing_dollar, readyInMinutes, summary, round(fat/servings) as fatPerServing, round(carbohydrates/servings) as carbsPerServing,
+    (SELECT DISTINCT recipeId, title, servings, image, round(pricePerServing/150, 1) as pricerPerServing_dollar, readyInMinutes, summary, round(fat/servings) as fatPerServing, round(carbohydrates/servings) as carbsPerServing,
     CASE
       WHEN (protein / servings) > 1000 THEN ROUND((protein / servings) / 100, 1)
       ELSE ROUND((protein / servings) / 20, 1)
@@ -70,8 +70,8 @@ var calorie_per_snack_min = (calorie_required / (num_meals+(num_snacks/2)))/2 - 
     ROUND(calories / servings, 1) AS calories_per_serving
     FROM recipe
     WHERE (calories / servings) BETWEEN ${calorie_per_dish_min} AND ${calorie_per_dish_max}
-    AND (pricePerServing/100) <= ${budget_meal}
-    AND recipeID IN (SELECT recipeID FROM diets WHERE name = '${diets}')
+    AND (pricePerServing/150) <= ${budget_meal}
+    AND recipeId IN (SELECT recipeId FROM diets WHERE name = '${diets}')
     ORDER BY RAND()
     LIMIT ${num_meals})`;
       db.query(sql)
